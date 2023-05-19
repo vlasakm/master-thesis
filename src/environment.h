5546f89a77063cc5c69df5c39bfa08ee7243b2b5
Michal Vlasák <vlasami6@fit.cvut.cz>
2023-05-19 17:21:23 +0200

    Move `Environment` to separate module

---
+++ src/environment.h
@@ -0,0 +1,18 @@
+#include "table.h"
+#include "ir.h"
+
+typedef struct {
+	Table *scopes;
+	size_t scope_cnt;
+	size_t scope_cap;
+} Environment;
+
+void env_push(Environment *env);
+
+void env_pop(Environment *env);
+
+void env_define(Environment *env, Str name, Value *value);
+
+bool env_lookup(Environment *env, Str name, void **value);
+
+void env_free(Environment *env);
--- src/parser.c
+++ src/parser.c
@@ -1,55 +1,6 @@
 #include "parser.h"
 #include "table.h"
-
-typedef struct {
-	Table *scopes;
-	size_t scope_cnt;
-	size_t scope_cap;
-} Environment;
-
-void
-env_push(Environment *env)
-{
-	if (env->scope_cnt == env->scope_cap) {
-		env->scope_cap = env->scope_cap ? env->scope_cap * 2 : 8;
-		GROW_ARRAY(env->scopes, env->scope_cap);
-	}
-	table_init(&env->scopes[env->scope_cnt++]);
-}
-
-void
-env_pop(Environment *env)
-{
-	assert(env->scope_cnt > 0);
-	table_free(&env->scopes[--env->scope_cnt]);
-}
-
-void
-env_define(Environment *env, Str name, Value *value)
-{
-	assert(env->scope_cnt > 0);
-	table_insert(&env->scopes[env->scope_cnt - 1], name, value);
-}
-
-bool
-env_lookup(Environment *env, Str name, void **value)
-{
-	for (size_t i = env->scope_cnt; i--;) {
-		if (table_get(&env->scopes[i], name, value)) {
-			return true;
-		}
-	}
-	return false;
-}
-
-void
-env_free(Environment *env)
-{
-	for (size_t i = 0; i < env->scope_cnt; i++) {
-		table_free(&env->scopes[--env->scope_cnt]);
-	}
-	FREE_ARRAY(env->scopes, env->scope_cnt);
-}
+#include "environment.h"
 
 #include "lexer.c"
 
